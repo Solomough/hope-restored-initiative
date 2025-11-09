f97419985ff44ee504a1f3685a4ca277d990294e
Solomon Moughkaa Zahemen <zahemenmoughkaa@gmail.com>
2025-11-09 14:33:26 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4BB4E6",
+        secondary: "#5BCDA0",
+        accent: "#DCF5F2"
+      }
+    },
+  },
+  plugins: [],
+}
